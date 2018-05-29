702135a912fe0f8a9da7651c32485b401d3758f5
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-05-29 23:46:02 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  post 'user_token' => 'user_token#create'
   resources :users
-  post 'user_token' => 'user#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   jsonapi_resources :posts
 end
